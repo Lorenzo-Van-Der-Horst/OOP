334fc58de999d16c62f0200ceca41e869b93264a
Lorenzo-Van-Der-Horst <112870039+Lorenzo-Van-Der-Horst@users.noreply.github.com>
2023-06-14 15:37:42 +0200

    Add files via upload

--- OOP5.php
+++ OOP5.php
@@ -2,7 +2,17 @@
     class Product
     {
         public $name;
+
         public $price;
+
+        public $category;
+
+        public function __construct($name, $price, $category)
+        {
+            $this->name = ucfirst($name);
+            $this->price = $price;
+            $this->category = strtoupper($category);
+        }
 
         public function formatPrice()
         {
@@ -10,49 +20,48 @@
         }
     }
 
-    $frisdrank1 = new Product();
-    $frisdrank1->name = "Frisdrank 1";
-    $frisdrank1->price = 40;
+    $fruit1 = new Product(name:"fruit 1", price:40, category:"category 1");
 
+    $fruit2 = new Product(name:"fruit 2", price:10, category:"category 2");
 
+    $fruit3 = new Product(name:"fruit 3", price:20, category:"category 3");
 
-    $frisdrank2 = new Product();
-    $frisdrank2->name = "Frisdrank 2";
-    $frisdrank2->price = 10;
-
-
-
-    $frisdrank3 = new Product();
-    $frisdrank3->name = "Frisdrank 3";
-    $frisdrank2->price = 20;
-    
-
-
-    $frisdrank4 = new Product();
-    $frisdrank4->name = "Frisdrank 4";
-    $frisdrank2->price = 30;
+    $fruit4 = new Product(name:"fruit 4", price:30, category:"category 4");
 
 
 
     echo $frisdrank1->formatPrice()."<br>";
+
     echo $frisdrank1->name."<br>";
+
     echo $frisdrank1->price."<br>";
+
+    echo $fruit1->category. "<br><br>";
 
 
 
     echo $frisdrank2->formatPrice()."<br>";
+
     echo $frisdrank2->name. "<br>";
+
     echo $frisdrank2->price."<br>";
+
+    echo $fruit2->category. "<br><br>";
 
 
 
     echo $frisdrank3->formatPrice()."<br>";
+
     echo $frisdrank3->name. "<br><br>";
+
     echo $frisdrank3->price."<br>";
 
+    echo $fruit3->category. "<br><br>";
 
 
-    $frisdrank1->name = "Frisdrank_1";
+
+    $fruit1->setName(name:"fruit_1");
+    
     echo $frisdrank1->name. "<br><br>";
 
 
@@ -62,6 +71,6 @@
     var_dump($frisdrank2);
 
     var_dump($frisdrank3);
-    /+
+    
     var_dump($frisdrank4);
 ?>
---
+++ OOP7.php
@@ -0,0 +1,34 @@
+<?php
+    class Product
+    {
+
+        public function __construct(public $price, public $name = "een muzieksoort", public $currency = "&euro")
+        {
+            $this->name = ucfirst($name);
+
+            $this->price = $price;
+
+            $this->currency = $currency;
+        }
+
+        public function formatPrice()
+        {
+            return number_format($this->price, decimals:2);
+        }
+    }
+
+    $muzieksoort1 = new Product(price:40, currency:"€", name:"muzieksoort 1");
+    
+    //$muzieksoort2 = new Product(price:10, name:"muzieksoort 2");
+
+//    echo "<br>".$muzieksoort1->name."<br>";
+//    echo $muzieksoort1->currency."<br>";
+//    echo $muzieksoort1->price."<br><br>";
+
+    //echo $muzieksoort2->formatPrice(). "<br>";
+    //echo $muzieksoort2->name. "<br>";
+    //echo $muzieksoort2->price. "<br><br>";
+    
+    var_dump($muzieksoort1);
+    //var_dump($muzieksoort2);
+?>
